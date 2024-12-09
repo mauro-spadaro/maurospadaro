f53b89e80dc635b9f5dc779350810be0ab642461
Mauro Spadaro <spadaro.mauro@gmail.com>
2024-12-09 11:28:01 +0100

    Added footer

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
           <body className="bg-gray-100">
               <Navbar />
               <main>{children}</main>
+              <Footer />
           </body>
       </html>
   );

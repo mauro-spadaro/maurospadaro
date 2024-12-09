5c96c6d3af078e0e05827bf60acc80ff644d51db
Mauro Spadaro <spadaro.mauro@gmail.com>
2024-12-09 12:35:00 +0100

    Added more content to the FE

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,11 @@
-export default function Articles() {
-  return (
-      <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-          <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>HOMEPAGE</h1>
-          <p style={{ fontSize: '1.2rem', lineHeight: '1.6' }}>
-              HERE THERE WILL THE LATEST ARTICLE
-          </p>
-      </div>
-  );
-}
+export default function Homepage() {
+    return (
+        <div className="p-8 font-sans bg-gray-200">
+            <h1 className="text-red-500 text-4xl font-bold">HOMEPAGE</h1>
+            <p className="text-lg mt-4 leading-relaxed">
+                HERE THERE WILL THE LATEST ARTICLE
+            </p>
+        </div>
+    );
+  }
+  

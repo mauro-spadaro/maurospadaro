439350934b38b72f7c2430ccfb3740ed6c3b5c5b
Mauro Spadaro <spadaro.mauro@gmail.com>
2024-12-09 14:22:40 +0100

    Reinitialised FE because Tailwind was not working

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,5 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,12 +19,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-      <html lang="en">
-          <body className="bg-gray-100">
-              <Navbar />
-              <main>{children}</main>
-              <Footer />
-          </body>
-      </html>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
   );
 }

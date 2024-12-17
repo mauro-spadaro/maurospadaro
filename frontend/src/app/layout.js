bbfcf3b1a9a15b38f7c780c798a013f7cbca6fd0
Mauro Spadaro <spadaro.mauro@gmail.com>
2024-12-17 23:20:52 +0100

    Added Navbar

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from './components/Navbar'; 
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* Add Navbar Here */}
+        <Navbar />
+
+        {/* Main Content */}
+        <main>{children}</main>
       </body>
     </html>
   );
